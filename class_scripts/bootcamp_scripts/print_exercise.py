548d898842c0734ab4b9311405ee63369908f026
Shaleka Hernandez <s.mica.h128@gmail.com>
2020-09-24 17:47:27 -0400

    this is my commit message i hope this works

---
+++ class_scripts/bootcamp_scripts/print_exercise.py
@@ -0,0 +1,42 @@
+# this line of code prints out a first line of text 
+print('hi, this is your computer speaking')
+# this line of code prints out a second line of text 
+print('i can run a lot of python code')
+
+# assigning the variable firstname with 'Paul'
+firstname = 'Paul'
+
+# print out contents of the variable 
+print (firstname)
+
+# intergers 
+print(1+3)
+print(1*3)
+print(1-3)
+
+# addition +
+# subtraction - 
+# multiplication * 
+# division / 
+# exponents ** 
+
+positive_number = 10 
+negative_number = -5 
+added = positive_number + negative_number
+
+print(added)
+
+# floats 
+print(1.1+3.1)
+print(1.5*2)
+print(1-3.736)
+
+# converting float to integer 
+print(int(3.0))
+print(int(5.3))
+print(int (2))
+
+# converting integer to float 
+print(float(7))
+
+# catching up on saving files 
