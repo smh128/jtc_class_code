5a51a6194892d0c60a52e73c53ec1cae803242e0
Shaleka Hernandez <s.mica.h128@gmail.com>
2020-09-24 17:46:28 -0400

    this is my commit message i hope this works

---
+++ strings_practice.py
@@ -0,0 +1,10 @@
+# Strings have text in them 
+# Strings are surronded by quotes 
+
+# assign a string variable 
+street = "Amsterdam Avenue"
+print (street)
+
+# lyrics, using escape characters 
+ 
+ 
