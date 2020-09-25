ef499ad4c5c6b18e44f17765cfb1dc45d03a931c
Shaleka Hernandez <s.mica.h128@gmail.com>
2020-09-24 21:54:37 -0400

    moving around a lot of stuff

---
+++ challenges/04_NBA /nba3pts.py
@@ -0,0 +1,60 @@
+
+print("Challenge 2.1:") 
+jamal_murray_3pts_made = 46
+Vanvleet_3pts_made = 43
+Harden_3pts_made = 39 
+
+print("Challenge 2.2:")
+print("In the 2020 NBA playoffs Jamal Murray made this many 3 point shots:")
+print(jamal_murray_3pts_made)
+print("In the 2020 NBA playoffs Fred Vanvleet made this many 3 point shots:")
+print(Vanvleet_3pts_made)
+print("In the 2020 NBA playoffs James Harden made this many 3 point shots:")
+print(Harden_3pts_made)
+
+print("Challenge 2.3: Store the number of three point shot attempts in variables for each player")
+Jamal_Murray_3pts_attempts = 130
+Vanvleet_3pts_attempts = 110 
+Harden_3pts_attempts = 117 
+
+print(Jamal_Murray_3pts_attempts)
+print(Vanvleet_3pts_attempts)
+print(Harden_3pts_attempts)
+
+print("Challenge 2.4: Build on your print statement")
+print("In the 2020 NBA playoffs Jamal Murray made this many 3 point attempts:")
+print(Jamal_Murray_3pts_attempts)
+print("In the 2020 NBA playoffs Fred Vanvleet made this many 3 point attempts:")
+print(Vanvleet_3pts_attempts)
+print("In the 2020 NBA playoffs James Harden made this many 3 point attempts:")
+print(Harden_3pts_attempts)
+# the number of three point shots for each player. E.g., output should be similar to
+# "In the 2020 NBA playoffs, player X made Y 3 point shots and Z 3 point shot attempts."
+print()
+
+print("Challenge 2.5: Calculate, store, and print the three point percentage for each player")
+# TODO: Calculate the three point percentage, which is given by `three points made/three point attempts`
+jamal_murray_3pts_percentage = 35
+Vanvleet_3pts_percentage = 39
+Harden_3pts_percentage = 33
+# TODO: Calculate and print the 3 point percentage for James Harden
+print("In the 2020 NBA playoffs James Harden had the following 3 point percentage:")
+print(Harden_3pts_percentage)
+
+print('Challenge 3.1: Print out the paragraph but with only 1 sentence per line')
+# TODO: Print the giant chunk of text out using escape characters so each sentence comes out on a new line
+
+print('Challenge 3.2: Print out the paragraph but with only 1 sentence per line')
+# TODO: As above, orint out the paragraph with only 1 sentence per line, and all in upper case
+
+print('Challenge 3.3: Make a boolean variable indicating whether you think the Lakers are the best NBA team')
+# TODO: make a variable called `lakers_are_best` to indicate this
+# TODO: print out the variable in an f-string to convey your opinion on the lakers
+
+print('Challenge 3.4: Type Conversion')
+# TODO: Convert your `lakers_are_best` variable to an integer, and print it out. 
+# TODO: Convert your `lakers_are best` variable to a float, and print it out
+
+print('Challenge 3.5: Type Conversion Part 2')
+# TODO: Take each player's three point percentage (from part 2.5) and convert it to a string, then print it out.
+# TODO: Take each player's three point percentage (from part 2.5) and convert it to an integer, then print it out.
